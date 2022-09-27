f4baf861c92915f9d8c0ac32bc3ac889b784521d
srhernandez1 <srhernandez1@LAPTOP-TIS6S1I7.localdomain>
2022-09-27 17:20:35 -0300

    prueba get airport 2

--- api.py
+++ api.py
@@ -68,7 +68,7 @@ async def get_airports():
 @app.get("/airports/{airport_id}",response_model = List[Airport])
 async def get_airports(airport_id):
     query = "SELECT * FROM airports WHERE id = :id"
-    return await database.fetch_one(query=query,values={"id":airport_id})
+    return await Airport(id = "a",name =  "a",country = "a",city = "a", position = {"a":2,"b":1})
 
 @app.post("/airports/",response_model = Airport)
 async def create_airports(airport: Airport):

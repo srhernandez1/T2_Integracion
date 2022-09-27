ed4f8ee565ab64663681c846abb95b02a150540e
srhernandez1 <srhernandez1@LAPTOP-TIS6S1I7.localdomain>
2022-09-27 17:55:18 -0300

    prueba get airport 7

--- api.py
+++ api.py
@@ -65,10 +65,10 @@ async def get_airports():
     query = airports.select()
     return await database.fetch_all(query)
 
-@app.get("/airports/{airport_id}",response_model = List[Airport])
+@app.get("/airports/{airport_id}",response_model = Airport)
 async def get_airports(airport_id):
     query = sqlalchemy.select(airports).where(airports.c.id == airport_id)
-    return await database.fetch_all(query)
+    return await database.fetch_one(query)
 
 @app.post("/airports/",response_model = Airport)
 async def create_airports(airport: Airport):

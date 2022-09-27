109d36306ae23ab1987ed393ba21a7503774c3b0
srhernandez1 <srhernandez1@LAPTOP-TIS6S1I7.localdomain>
2022-09-27 01:22:24 -0300

    prueba post 4

--- api.py
+++ api.py
@@ -51,9 +51,6 @@ class Flight(BaseModel):
 @app.on_event("startup")
 async def startup():
     await database.connect()
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
 
 @app.get("/")
 def index():

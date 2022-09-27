5f47f2d606f253ed003fb8c2474da3905216a7da
srhernandez1 <srhernandez1@LAPTOP-TIS6S1I7.localdomain>
2022-09-26 23:28:43 -0300

    prueba api 1

--- api.py
+++ api.py
@@ -1,71 +1,99 @@
 from fastapi import FastAPI, Path
 from typing import Optional
 from pydantic import BaseModel
+import databases
+import sqlalchemy
+from sqlalchemy.dialects.postgresql import JSON
 
+database_url = "postgres://rsxmhaipetxgsy:6c3eca151f1bf454f58b6a833f9b96b6766365baa97868b9964d8adf60fc6281@ec2-54-91-223-99.compute-1.amazonaws.com:5432/d69ei6qdr31e4i"
+database = databases.Database(database_url)
+metadata = sqlalchemy.MetaData()
+
+airports = sqlalchemy.Table(
+    "airports",
+    metadata,
+    sqlalchemy.Column("id",sqlalchemy.String, primary_key = True),
+    sqlalchemy.Column("name",sqlalchemy.String),
+    sqlalchemy.Column("country",sqlalchemy.String),
+    sqlalchemy.Column("city",sqlalchemy.String),
+    sqlalchemy.Column("position",JSON),
+)
+
+flights = sqlalchemy.Table(
+    "flights",
+    metadata,
+    sqlalchemy.Column("id",sqlalchemy.String, primary_key = True),
+    sqlalchemy.Column("departure",sqlalchemy.String),
+    sqlalchemy.Column("destination",sqlalchemy.String),
+)
+
+engine = sqlalchemy.create_engine(
+    database_url
+)
+metadata.create_all(engine)
 
 app = FastAPI()
 
-students = {
-    1: {
-        "name": "john",
-        "age": 17,
-        "year": "year 12"
-    }
-}
-
-class Student(BaseModel):
+class Airport(BaseModel):
+    id: str
     name: str
-    age: int
-    year: str
+    country: str
+    city: str
+    position: JSON
 
-class UpdateStudent(BaseModel):
-    name: Optional[str] = None
-    age: Optional[int] = None
-    year: Optional[str] = None
+class Flight(BaseModel):
+    id: str
+    departure: str
+    destination: str
 
 @app.get("/")
 def index():
     return {"name": "First Data"}
 
-@app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
-    return students[student_id]
+@app.get("/get-airports/")
+async def get_airports():
+    query = "SELECT * FROM airports"
+    return await database.fetch_all(query = query)
 
-@app.get("/get-by-name/{student_id}")
-def get_student(*, student_id: int, name: Optional[str] = None, test : int):
-    for student_id in students:
-        if students[student_id]["name"] == name:
-            return students[student_id]
-    return {"Data": "Not found"}
+# @app.get("/get-airport/{airport_id}")
+# def get_airport(airport_id):
+#     return students[student_id]
 
-@app.post("/create-student/{student_id}")
-def create_student(student_id : int, student : Student):
-    if student_id in students:
-        return {"Error": "Student exists"}
+# @app.get("/get-by-name/{student_id}")
+# def get_student(*, student_id: int, name: Optional[str] = None, test : int):
+#     for student_id in students:
+#         if students[student_id]["name"] == name:
+#             return students[student_id]
+#     return {"Data": "Not found"}
 
-    students[student_id] = student
-    return students[student_id]
+# @app.post("/create-student/{student_id}")
+# def create_student(student_id : int, student : Student):
+#     if student_id in students:
+#         return {"Error": "Student exists"}
 
-@app.put("/update-student/{student_id}")
-def update_student(student_id: int, student: UpdateStudent):
-    if student_id not in students:
-        return {"Error": "Student does not exist"}
+#     students[student_id] = student
+#     return students[student_id]
 
-    if student.name != None:
-        students[student_id].name = student.name
+# @app.put("/update-student/{student_id}")
+# def update_student(student_id: int, student: UpdateStudent):
+#     if student_id not in students:
+#         return {"Error": "Student does not exist"}
 
-    if student.age != None:
-        students[student_id].age = student.age
+#     if student.name != None:
+#         students[student_id].name = student.name
 
-    if student.year != None:
-        students[student_id].year = student.year
+#     if student.age != None:
+#         students[student_id].age = student.age
 
-    return students[student_id]
+#     if student.year != None:
+#         students[student_id].year = student.year
 
-@app.delete("/delete-student/{student_id}")
-def delete_student(student_id: int):
-    if student_id not in students:
-        return {"Error": "Student does not exist"}
+#     return students[student_id]
 
-    del students[student_id]
-    return {"Message": " Student deleted successfully"}
+# @app.delete("/delete-student/{student_id}")
+# def delete_student(student_id: int):
+#     if student_id not in students:
+#         return {"Error": "Student does not exist"}
+
+#     del students[student_id]
+#     return {"Message": " Student deleted successfully"}

44834cce58deb14181018b6915c80f90b8effa27
srhernandez1 <srhernandez1@LAPTOP-TIS6S1I7.localdomain>
2022-09-28 15:47:06 -0300

    probando errors 30

--- api.py
+++ api.py
@@ -102,8 +102,8 @@ async def get_airports(airport_id):
     err = await database.fetch_one(query_err)
     if err ==None:
         return JSONResponse(
-            status_code=400,
-            content="Ta malo",
+            status_code=404,
+            content="Airport with id "+str(airport_id)+" not found",
         )
     return await database.fetch_one(query)
 @app.get("/flights/{flight_id}",response_model = Flight)

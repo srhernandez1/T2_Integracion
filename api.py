41c63000c9101770cb0dded4bdce2897584ab432
srhernandez1 <srhernandez1@LAPTOP-TIS6S1I7.localdomain>
2022-09-28 16:50:16 -0300

    probando errors 35

--- api.py
+++ api.py
@@ -121,7 +121,7 @@ async def get_airports(flight_id):
 
 
 @app.post("/airports",response_model = Airport,status_code = 201)
-async def create_airports(airport: dict):
+async def create_airports(airport: Airport):
     for field in airport.__fields__:
         if not isinstance(getattr(airport,field),str) and field != "position":
             return JSONResponse(
